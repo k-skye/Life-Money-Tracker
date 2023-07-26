6231e047432512fc9bd2183362198047b73ca649
kang <knskye@gmail.com>
2023-07-26 21:46:07 +0800

    Initial Commit

---
+++ Life&MoneyTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Life&MoneyTracker
+//
+//  Created by kang on 26/7/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
